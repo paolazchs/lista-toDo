5de8a9a66100ad2fbb00f1ac77ef3d13652aca01
Paola de Oliveira <paola9liveira06@gmail.com>
2025-08-12 15:35:34 -0300

    interface

--- src/app/lista-to-do/lista-to-do.ts
+++ src/app/lista-to-do/lista-to-do.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-to-do',
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './lista-to-do.html',
   styleUrl: './lista-to-do.scss'
 })
 export class ListaToDo {
-
+  itemInput: string = '';
+  
 }
